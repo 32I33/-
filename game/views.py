970a98d2ce6ec8f04ffd3745b04e83b9e4fb8aa1
Rexj <528764477@qq.com>
2022-03-24 14:56:37 +0800

    first try

--- game/views.py
+++ game/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    a = 1
+    b = 2
+    total = a + b
+
+    line = '<h1 style="text-align: center">欢迎来到炉石传说</h1>'
+    gotoPlay = '<a href="/game/">进入游戏界面</a>'
+    image = '<image src="https://blz.nosdn.127.net/1/tm/hearthstone/gameguide/voyage-to-the-sunken-city/minisite/logo.png" width=2000 />'
+
+    return HttpResponse(line + gotoPlay + '<hr>'  + image)
+
+def play(request):
+    start = '<h1 style="text-align: center">游戏界面</h1>'
+    goback = '<a href="/">返回主页面</a>'
+    return HttpResponse(start + goback)
+
